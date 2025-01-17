d32c909f308c4b53b9ba7c646ffa96a5c6e59726
dashhy <robertblendea@outlook.com>
2025-01-17 11:47:17 -0800

    Initial Commit

--- src/Main.kt
+++ src/Main.kt
@@ -6,17 +6,20 @@ fun main() {
     // UNTRANSLATED: We'll use readline later to get the data.
 
     // Ask the user for a word
+    println("enter your word here")
 
     // Declare a String named enteredTerm and initialize it to the next word
     // read in from getData
-
+    var enteredTerm = readlnOrNull()
 
     // Clear out the buffer by calling nextLine on getData. UNTRANSLATED.
 
     // Declare a String with an identifier convertedTerm, initialized to an empty String
+    var convertedTerm = ""
 
     // Declare a String with an identifier firstLetter, initialized to a substring of
     // enteredWord starting at index 0 and ending at index 1. We'll use substring as normal
+    var firstLetter = enteredTerm?.substring(0,1)
 
     // If firstLetter is a vowel (note - this is five conditions using OR logic)
         //Set convertedTerm to enteredterm concatenated with "yay"
@@ -25,9 +28,14 @@ fun main() {
         // with a substring of enteredTerm from index 0 to index 1, concatenated with "ay"
     // TRANSLATED: When becomes a better control structure to use here
 
+    convertedTerm = when (firstLetter) {
+        "a", "e", "i", "o", "u" -> enteredTerm + "yay"
+        else -> enteredTerm?.substring(1) + enteredTerm?.substring(0, 1) + "ay"
     }
+
     // Print that the Pig Latin Version of the enteredTerm is your convertedTerm
 
+    println("The Pig Latin Version of $enteredTerm is $convertedTerm")
 
 
 
